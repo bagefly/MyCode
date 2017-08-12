a331886f9bccda83c1733e26ecd612126e092737
bagefly <huangxhe@126.com>
2017-08-12 18:08:47 +0800

    js_对象&数组

--- 20170811/.main2.js
+++ 20170811/.main2.js
@@ -6,17 +6,51 @@ var height = 450;
 var upToDown = true;
 var leftToRight = true;
 var Vz = 5;
-var angle = Math.random*(2*Math.PI);
+var angle = Math.random()*(2*Math.PI);
+var count = 0;
 setInterval(function(){
-    var yV = Vz*Math.sin(angle)
-    if(!(dotTop > height-50 && yV>0) && !(dotTop <0 && yV <0)){    
-        dotTop += yV;
-        dot.style.top = dotTop + "px" ;
+    count++;
+    var yV = Vz*Math.sin(angle) ;
+    var xV = Vz*Math.cos(angle) ;
+    if(upToDown){
+        dotTop += yV ;
+        if(dotTop > height){
+            dot.style.top = height + "px";
+        }else{
+            dot.style.top = dotTop + "px";
+        }
+    }else{
+        dotTop -= yV ;
+        if(dotTop < 0){
+            dot.style.top = 0;
+        }else{
+            dot.style.top = dotTop + "px";
+        }
     }
-    var xV = Vz*Math.cos(angle);
-    if(!(dotLeft >width-50 && xV>0) && !(dotLeft <0 && xV <0)){
-        dotLeft += xV;
-        dot.style.Left = dotLeft + "px" ;
+    if(leftToRight){
+        dotLeft += xV ;
+        if(dotLeft > width){
+            dot.style.left = width + "px";
+        }else{
+            dot.style.left = dotLeft + "px";
+        }
+    }else{
+        dotLeft -= xV ;
+        if(dotLeft < 0){
+            dot.style.left = 0;
+        }else{
+            dot.style.left = dotLeft + "px";
+        }
+
+    }
+    if(dotTop > height || dotTop < 0){
+        upToDown = ! upToDown;
+    }
+    if(dotLeft > width || dotLeft < 0){
+        leftToRight = ! leftToRight;
+    }
+    if(count % 100 == 0){
+        angle = Math.random()*(2*Math.PI) ;
     }
 },10)
 
