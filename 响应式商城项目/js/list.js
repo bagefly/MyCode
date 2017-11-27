0080aa11103915d567a32f8c4c1e043ca5f772ab
bagefly <huangxhe@126.com>
2017-11-27 20:20:20 +0800

    响应式项目（end）

--- 响应式商城项目/js/list.js
+++ 响应式商城项目/js/list.js
@@ -42,9 +42,60 @@ function showShop(page,cat_id){
                 $("#goodList").append('<li class="col-md-3 col-sm-6 col-xs-6"><img src="'+obj.data[i].goods_thumb+'"alt=""/><p class="goods_name"><a href="detail.html?goods_id='+obj.data[i].goods_id+'">'+ obj.data[i].goods_name+'</a></p><p class="price">￥' +obj.data[i].price+  '</p></li>');
             }
             //分页
-            for(var j=0;j<18;j++){
-                $("#ButtonCenter").append( $('<span>'+(j+1)+'</span>') );
-                $("#ButtonCenter span").eq(page-1).css("backgroundColor","#ff9100").siblings().css("backgroundColor","");
+            var maxPage =response.page.page_count;
+            if(!maxPage){
+                layer.open({
+                    content: '暂无该类商品',
+                    btn: ["确定"],
+                    btn1: function () {
+                        location.href = "list.html";
+                    },
+                    scrollbar: false
+                });
+            }
+
+
+            if(maxPage<6){
+                for(var i=0;i<=maxPage;i++){
+                    $("#ButtonCenter span").eq(i).html(i+1);
+                }
+                for(var i=maxPage;i<=6;i++){
+                    $("#ButtonCenter span").eq(i).hide();
+                }
+               
+            }
+
+            if(page >= 1 && page <= 3){
+                $("#ButtonCenter span").eq(0).html("1");
+                $("#ButtonCenter span").eq(1).html("2");
+                $("#ButtonCenter span").eq(2).html("3");
+                $("#ButtonCenter span").eq(3).html("4");
+                $("#ButtonCenter span").eq(4).html("…");
+                $("#ButtonCenter span").eq(5).html(maxPage-1);
+                $("#ButtonCenter span").eq(6).html(maxPage);
+                //cur
+                $("#ButtonCenter span").eq(page - 1).addClass("page-cur").siblings().removeClass("page-cur");
+            }
+            else if(page <= maxPage && page >=  maxPage - 2){
+                $("#ButtonCenter span").eq(0).html("1");
+                $("#ButtonCenter span").eq(1).html("2");
+                $("#ButtonCenter span").eq(2).html("…");
+                $("#ButtonCenter span").eq(3).html(maxPage-3);
+                $("#ButtonCenter span").eq(4).html(maxPage-2);
+                $("#ButtonCenter span").eq(5).html(maxPage-1);
+                $("#ButtonCenter span").eq(6).html(maxPage);
+                //cur
+                $("#ButtonCenter span").eq(page - maxPage - 1).addClass("page-cur").siblings().removeClass("page-cur");
+            }else{
+                $("#ButtonCenter span").eq(0).html("1");
+                $("#ButtonCenter span").eq(1).html("…");
+                $("#ButtonCenter span").eq(2).html(page - 1);
+                $("#ButtonCenter span").eq(3).html(page);
+                $("#ButtonCenter span").eq(4).html(page + 1);
+                $("#ButtonCenter span").eq(5).html("…");
+                $("#ButtonCenter span").eq(6).html(maxPage);
+                //cur
+                $("#ButtonCenter span").eq(3).addClass("page-cur").siblings().removeClass("page-cur");
             }
         }
     });
@@ -57,8 +108,7 @@ $("#ButtonPrev").click(function(){
     //范围
     if(page <= 1) page = 1;
     //清空数据
-    $("#goodList").html('s');
-    $("#ButtonCenter").html('');
+    $("#goodList").html('');
     showShop(page,cat_id);
     
 })
@@ -66,11 +116,9 @@ $("#ButtonPrev").click(function(){
 $("#ButtonNext").click(function(){
     //信号量增加
     page++;
-    //范围
-    if(page >= 18) page = 18;
+    
     //清空数据
     $("#goodList").html('');
-    $("#ButtonCenter").html('');
     showShop(page,cat_id);
     
 })
@@ -82,10 +130,12 @@ $("#ButtonCenter").ready(function(){
     var target = event.target || event.srcElement;
 //	console.log(target.nodeName)
     if( target.nodeName === "SPAN" ){
+        if(target.innerHTML == "…"){
+            return;
+        }
         //得到当前分页的内容，存入信号量
-        page = target.innerText;
+        page = parseInt(target.innerText);
         $("#goodList").html('');
-        $("#ButtonCenter").html('');
         showShop(page,cat_id);
     
     }
